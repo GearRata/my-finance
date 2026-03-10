3943af19f445fcb74695376b10040e47c15141bb
Weerawut <gearmcc2545@gmail.com>
2026-03-10 23:07:55 +0700

    feat:add login page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,11 @@
-
+import { Button } from "@/components/ui/button"
+import { LoginForm } from "@/components/login-form";
 export default function Home() {
   return (
-   <h1>Hello</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-4xl flex-col gap-6 ">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
